cfe435e450a62b631e6062035ddd4c9307320e70
Mitch Lum <mklum88@gmail.com>
2020-01-08 23:01:33 +0000

    added fake data to tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password) 
+VALUES ('Mitch Lum','mklum88@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('User McUserface','user@gmail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dan Ballard', 'dan.ballard@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
